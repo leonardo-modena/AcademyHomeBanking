b21207decfedaf48fc1bf8f3c5be8adc76fd979e
omar191096 <omarstar960@gmail.com>
2021-08-26 12:36:39 +0200

    listByRuolo

--- BackEnd/project/src/main/java/com/banking/project/dao/CustomerRepository.java
+++ BackEnd/project/src/main/java/com/banking/project/dao/CustomerRepository.java
@@ -2,9 +2,18 @@ package com.banking.project.dao;
 import com.banking.project.entity.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.awt.print.Pageable;
+import java.util.Collection;
+import java.util.List;
 
 @RepositoryRestResource(path = "customers")
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
+
+    @Query(value = "select * from utenti  where ruolo = :ruolo", nativeQuery = true)
+    Customer findAllByRolename(String ruolo);
 }
