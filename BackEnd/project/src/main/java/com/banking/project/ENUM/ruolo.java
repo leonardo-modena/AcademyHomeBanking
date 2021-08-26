dfcfdea8003a4d0242bb4269fecc325fa271017e
SoniaRestuccia <83927927+SoniaRestuccia@users.noreply.github.com>
2021-08-26 11:09:44 +0200

    Added Project

---
+++ BackEnd/project/src/main/java/com/banking/project/ENUM/ruolo.java
@@ -0,0 +1,5 @@
+package com.banking.project.ENUM;
+
+public  enum ruolo {
+    C,D
+}
---
+++ BackEnd/project/src/main/java/com/banking/project/entity/Customer.java
@@ -0,0 +1,96 @@
+package com.banking.project.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="utenti")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "nome")
+    private String firstName;
+
+    @Column(name = "cognome")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column (name = "ruolo")
+    private String ruolo ;
+
+    public Customer(){
+
+    }
+
+    public Customer(String firstName, String lastName, String email,String password,String ruolo) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password=password;
+        this.ruolo= ruolo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+
+	@Override
+	public String toString() {
+		return "{id:" + id + ", nome:" + firstName + ", cognome:" + lastName + ", email:" + email + ", password:"
+				+ password + ", ruolo:" + ruolo + "}";
+	}
+    
+    
+}
