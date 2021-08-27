de80e184e6ac5df1f3582553b831d4159e8d0c47
leomod272 <leomod272@gmail.com>
2021-08-27 15:38:04 +0200

    admin panel responsive restyle

--- FrontEnd/src/app/components/admin/admin.component.ts
+++ FrontEnd/src/app/components/admin/admin.component.ts
@@ -1,21 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AdminService } from 'src/app/services/admin.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  adminInfo!: { nome: string; cognome: string };
 
-  adminInfo!: {nome: string, cognome: string};
+  userSection!: boolean;
+  operationSection!: boolean;
 
-  constructor(private adminService: AdminService) { }
+  sectionSelector!: boolean;
 
-  ngOnInit(): void {
-    this.adminService.actualAdmin.subscribe( (admin) => {
-      this.adminInfo = admin;
-    } )
+  constructor(private adminService: AdminService) {
+    this.responsiveSection();
   }
 
+  ngOnInit(): void {
+    this.adminService.actualAdmin.subscribe((admin) => {
+      this.adminInfo = admin;
+    });
+  }
+
+  userSectionClick(): void {
+    console.log('ciao');
+    this.operationSection = false;
+    this.userSection = true;
+  }
+
+  operationSectionClick(): void {
+    this.userSection = false;
+    this.operationSection = true;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  responsiveSection(event?: any) {
+    let screenWidth = window.innerWidth;
+    if (screenWidth > 767) {
+      this.userSection = true;
+      this.operationSection = true;
+      this.sectionSelector = false;
+    } else {
+      this.userSection = true;
+      this.operationSection = false;
+      this.sectionSelector = true;
+    }
+  }
 }
