fc8e1050905a6fbcac5a02feacfe4a1aa5a4114b
murae <e.mura@campus.unimib.it>
2021-08-27 11:10:35 +0200

    No operation message

--- FrontEnd/src/app/components/user/user.component.ts
+++ FrontEnd/src/app/components/user/user.component.ts
@@ -34,10 +34,6 @@ export class UserComponent implements OnInit {
 
   constructor(private userService: UserService, private router: Router) {  }
 
-  list = [
-    {tipo: 'versamento', importo: '30,52', data: 1629410400000, mittente: "Mamma", id: 1},
-    {tipo: 'prelievo', importo: '22,43', data: 1629410400000, beneficiario: "Panificio Nicolò", id: 2}
-  ]
   ngOnInit(): void {
     this.routeUrl = this.router.url;
     this.userService.user.subscribe((user: { nome: string, cognome: string, dataDiNascita: number, email: string, id: string }) => {
