025111a4f7abcabb679ab807257316d87e5ed27e
omar191096 <89470913+omar191096@users.noreply.github.com>
2021-09-02 09:47:37 +0200

    account-management-service

---
+++ BackEnd/account-management-service/account-management-service/src/main/java/com/banking/project/accountmanagementservice/repository/BankAccountRepository.java
@@ -0,0 +1,35 @@
+package com.banking.project.accountmanagementservice.repository;
+
+import com.banking.project.accountmanagementservice.entity.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount,Integer> {
+	
+	@Query(value = "select * from  bank_account b where b.account_status = 'INACTIVE'",nativeQuery = true)
+    List<BankAccount> findInactive();
+	
+	@Query(value = "select * from bank_account where account_status = 'CLOSING'", nativeQuery = true)
+    List<BankAccount> findClosing();
+
+	@Transactional
+	@Modifying(clearAutomatically = true)
+    @Query("update BankAccount b set b.account_status= 'ACTIVE' where b.id=:accountId")
+    void activeAccount(@Param("accountId")int accountId);
+
+	 /** Query che cancella l'account indicato dall'id
+	 * @param accountId
+	 */
+
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("delete BankAccount b where b.id=:accountId and b.account_status='CLOSING'")
+    void deleteAccount(@Param("accountId")int accountId);
+
+
+}
