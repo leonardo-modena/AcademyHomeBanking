abf56f51661f993881202c7e9d6c1642a855909d
SoniaRestuccia <sonia.restuccia1994@gmail.com>
2021-09-03 15:38:23 +0200

    CustomerController method apertura

--- BackEnd/account-management-service/src/main/java/com/banking/project/accountmanagementservice/controller/CustomerAccountManagementServiceController.java
+++ BackEnd/account-management-service/src/main/java/com/banking/project/accountmanagementservice/controller/CustomerAccountManagementServiceController.java
@@ -1,5 +1,6 @@
 package com.banking.project.accountmanagementservice.controller;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +17,43 @@ import com.banking.project.accountmanagementservice.entity.Customer;
 import com.banking.project.accountmanagementservice.repository.BankAccountRepository;
 import com.banking.project.accountmanagementservice.repository.CustomerRepository;
 
-
+import javassist.NotFoundException;
 
 @RestController
 @RequestMapping("/customer")
-public class CustomerAccountManagementServiceController{
+public class CustomerAccountManagementServiceController {
 	@Autowired
 	private BankAccountRepository bankAccountRepository;
-	
+
 	@Autowired
 	private CustomerRepository customerRepository;
-	
-	
 
 	/**
-	 * Metodo che richiede la chiusura dell'account da parte dell'utente, setta lo stato a "CLOSING"
+	 * Metodo che richiede la chiusura dell'account da parte dell'utente, setta lo
+	 * stato a "CLOSING"
+	 * 
 	 * @param accountId
 	 */
-	
+
 	@PutMapping(value = "/closingRequest/{accountId}")
 	public void closingAccount(@PathVariable int accountId) {
 		bankAccountRepository.closingRequest(accountId);
 	}
+
 	/**
-	 * Metodo per aprire un secondo conto
-	 * @param bankAccount
+	 * Metodo per recuperare le informazioni di un conto con l'id
+	 * 
+	 * @param
 	 * @return
 	 */
-	
-	@GetMapping("/profile/{customerId}")
-	public Optional <Customer> getCustomer(@PathVariable int customerId) {
 
-		
-		 return customerRepository.findById(customerId);
-		
-		
+	@GetMapping("/profile/{customerId}")
+	public Optional<Customer> getCustomer(@PathVariable int customerId) {
+
+		return customerRepository.findById(customerId);
+
 	}
-	
+
 	@GetMapping("/profile/bankAccount/{myBankAccount}")
 	public Optional<BankAccount> getMyBankAccount(@PathVariable int myBankAccount) {
 
@@ -60,24 +61,43 @@ public class CustomerAccountManagementServiceController{
 
 		return theBankAccount;
 	}
-	
-	
-	// da terminare
-	@PostMapping("/new")
-	public BankAccount newAccount(@RequestBody BankAccount bankAccount){
-		
-		if((bankAccount.getHolder().getBankAccounts().size()==1){
-			
-			int idFirstBankAccount = bankAccount.getHolder().getBankAccounts().get(0);
-			
-			bankAccountRepository.findById(idFirstBankAccount);
-		}
-		
-		bankAccount.setId(0);
-		
-		bankAccountRepository.save(bankAccount);
 
-		return bankAccount;
+	// da terminare
+	@PostMapping("/new/{id}/{balance}")
+	public String newAccount(@PathVariable int id, @PathVariable BigDecimal balance) {
+
+		BankAccount newBankAccount = new BankAccount();
+
+		if (bankAccountRepository.existsById(id)) {
+
+			BankAccount bankAccount = bankAccountRepository.getById(id);
+
+			if (((bankAccount.getBalance().compareTo(balance))>=0) && bankAccount.getAccount_status() == "ACTIVE") {
+
+				BigDecimal newOldBalance = bankAccount.getBalance().subtract(balance);
+
+				bankAccount.setBalance(newOldBalance);
+
+				newBankAccount.setId(0);
+
+				newBankAccount.setBalance(balance);
+
+				newBankAccount.setAccount_status("ACTIVE");
+
+				newBankAccount.setHolder(bankAccount.getHolder());
+
+				bankAccountRepository.save(newBankAccount);
+			} else {
+
+				return "Operazione negata";
+			}
+
+		} else {
+
+			return "Conto non trovato";
+		}
+
+		return "Creazione nuovo conto";
 	}
 
 }
