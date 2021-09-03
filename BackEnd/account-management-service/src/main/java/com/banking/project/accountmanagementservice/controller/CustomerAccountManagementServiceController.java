c569a66500d254a6bd4576ccf7c6546e2fd7bae6
SoniaRestuccia <sonia.restuccia1994@gmail.com>
2021-09-03 12:47:33 +0200

    customercontroller class

--- BackEnd/account-management-service/src/main/java/com/banking/project/accountmanagementservice/controller/CustomerAccountManagementServiceController.java
+++ BackEnd/account-management-service/src/main/java/com/banking/project/accountmanagementservice/controller/CustomerAccountManagementServiceController.java
@@ -1,6 +1,9 @@
 package com.banking.project.accountmanagementservice.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -9,8 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banking.project.accountmanagementservice.entity.BankAccount;
+import com.banking.project.accountmanagementservice.entity.Customer;
 import com.banking.project.accountmanagementservice.repository.BankAccountRepository;
 import com.banking.project.accountmanagementservice.repository.CustomerRepository;
+
+
 
 @RestController
 @RequestMapping("/customer")
@@ -20,6 +26,8 @@ public class CustomerAccountManagementServiceController{
 	
 	@Autowired
 	private CustomerRepository customerRepository;
+	
+	
 
 	/**
 	 * Metodo che richiede la chiusura dell'account da parte dell'utente, setta lo stato a "CLOSING"
@@ -27,7 +35,7 @@ public class CustomerAccountManagementServiceController{
 	 */
 	
 	@PutMapping(value = "/closingRequest/{accountId}")
-	public void activeAccount(@PathVariable int accountId) {
+	public void closingAccount(@PathVariable int accountId) {
 		bankAccountRepository.closingRequest(accountId);
 	}
 	/**
@@ -36,10 +44,34 @@ public class CustomerAccountManagementServiceController{
 	 * @return
 	 */
 	
+	@GetMapping("/profile/{customerId}")
+	public Optional <Customer> getCustomer(@PathVariable int customerId) {
+
+		
+		 return customerRepository.findById(customerId);
+		
+		
+	}
+	
+	@GetMapping("/profile/bankAccount/{myBankAccount}")
+	public Optional<BankAccount> getMyBankAccount(@PathVariable int myBankAccount) {
+
+		Optional<BankAccount> theBankAccount = bankAccountRepository.findById(myBankAccount);
+
+		return theBankAccount;
+	}
+	
+	
 	// da terminare
 	@PostMapping("/new")
 	public BankAccount newAccount(@RequestBody BankAccount bankAccount){
 		
+		if((bankAccount.getHolder().getBankAccounts().size()==1){
+			
+			int idFirstBankAccount = bankAccount.getHolder().getBankAccounts().get(0);
+			
+			bankAccountRepository.findById(idFirstBankAccount);
+		}
 		
 		bankAccount.setId(0);
 		
@@ -49,6 +81,3 @@ public class CustomerAccountManagementServiceController{
 	}
 
 }
-
-
-
