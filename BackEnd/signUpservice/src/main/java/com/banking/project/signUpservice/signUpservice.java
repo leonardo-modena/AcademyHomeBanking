40a68fc453515dd1484c8819d0ddeac0c41335fe
SoniaRestuccia <sonia.restuccia1994@gmail.com>
2021-09-08 10:18:13 +0200

    swagger v2

--- BackEnd/signUpservice/src/main/java/com/banking/project/signUpservice/signUpservice.java
+++ BackEnd/signUpservice/src/main/java/com/banking/project/signUpservice/signUpservice.java
@@ -3,7 +3,10 @@ package com.banking.project.signUpservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class signUpservice {
 
 	public static void main(String[] args) {
