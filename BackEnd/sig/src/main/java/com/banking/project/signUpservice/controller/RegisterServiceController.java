dcef367eda5626391199d2caa271fb598099a6d5
omar191096 <omarstar960@gmail.com>
2021-09-09 11:01:16 +0200

    signUpservice v3

--- BackEnd/sig/src/main/java/com/banking/project/signUpservice/controller/RegisterServiceController.java
+++ BackEnd/sig/src/main/java/com/banking/project/signUpservice/controller/RegisterServiceController.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping("/signup")
