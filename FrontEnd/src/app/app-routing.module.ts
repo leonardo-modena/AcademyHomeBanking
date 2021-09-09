775b9df7ec0bc4fac0b4aba7d46f5a465ca0891f
Giuseppe Zaccaria <3bzaccaria.giuseppe@gmail.com>
2021-09-09 09:48:28 +0200

    can activate registration

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { LoginComponent } from "./components/login/login.component";
 import { AdminComponent } from "./components/admin/admin.component";
 import { NotFoundComponent } from "./layout/not-found/not-found.component";
 import {AdminGuard} from "./guard/admin.guard";
+import {AuthGuard} from "./guard/auth.guard";
 
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' },
-  { path: 'registrazione', component: RegistrationComponent, pathMatch: 'full'},
+  { path: 'registrazione',canActivate: [AuthGuard], component: RegistrationComponent, pathMatch: 'full'},
   { path: 'login', component: LoginComponent, pathMatch: 'full' },
   { path: 'user', pathMatch: 'full' ,  loadChildren: () => import('./module/user/user.module').then(m => m.UserModule)},
   { path: 'admin',pathMatch: 'full', component: AdminComponent,  canActivate: [AdminGuard], loadChildren: () => import('./module/admin/admin.module').then(m => m.AdminModule) },
