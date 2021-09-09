24ce25c6ad1a3401d1b28a97853f404bde97c8df
omar191096 <omarstar960@gmail.com>
2021-09-09 15:19:35 +0200

    modifiche  generali
    modifiche  generali

---
+++ BackEnd/signUp-service/src/test/java/com/banking/project/signUpservice/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.project.signUpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
